2e8704f9e4a89feadedd63a113b21e40d55c47f6
sarahcheatham <sarah.cheatham14@gmail.com>
2019-05-15 11:38:08 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      weatherData: []
+    }
+  }
+  componentDidMount(){
+    const lat = 29.447705;
+    const lng = -94.658790;
+    let options = {
+      headers: {
+        'Authorization': 'a3e150ac-76c6-11e9-bf86-0242ac130004-a3e151a6-76c6-11e9-bf86-0242ac130004'
+      }
+    }
+    fetch(`https://api.stormglass.io/v1/weather/point?lat=${lat}&lng=${lng}`, options) 
+    .then(response => response.json())
+    .then(weatherData => this.setState({ weatherData }))
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>Bolivar Peninsula</h1>
+      </div>
+    );
+  }
 }
+
 
 export default App;
